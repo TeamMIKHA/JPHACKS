c46a8fe551fd6ed2804fcdc2dac731b6db5ef073
TeamMIKHA <73453598+TeamMIKHA@users.noreply.github.com>
2020-10-31 22:25:59 +0900

    Add files via upload

---
+++ wakeuptools.com/wakeup/forms.py
@@ -0,0 +1,52 @@
+from django import forms
+from .models import Schedule,Options
+
+
+class BS4ScheduleForm(forms.ModelForm):
+    """Bootstrapに対応するためのModelForm"""
+
+    class Meta:
+        model = Schedule
+        fields = ('summary', 'start_time')
+        widgets = {
+            'summary': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+
+            'start_time': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+        }
+
+    def clean_end_time(self):
+        start_time = self.cleaned_data['start_time']
+        end_time = self.cleaned_data['end_time']
+        if end_time <= start_time:
+            raise forms.ValidationError(
+                '終了時間は、開始時間よりも後にしてください'
+            )
+        return end_time
+
+
+class SimpleScheduleForm(forms.ModelForm):
+    """シンプルなスケジュール登録用フォーム"""
+
+    class Meta:
+        model = Schedule
+        fields = ('summary', 'start_time', 'date',)
+        widgets = {
+            'summary': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'start_time': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'date': forms.HiddenInput,
+        }
+
+class OptionsForm(forms.ModelForm):
+    category = forms.ModelChoiceField(queryset=Options.objects.all())
+
+    class Meta:
+        model = Options
+        fields = ('default_alarm', 'default_time', 'method_servo', 'method_sound',)
